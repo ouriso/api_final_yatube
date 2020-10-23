0308ada6b55b2e8f34ba43f40b582c95901cfe7c
Артур Гумеров <arturgumerov@yandex.ru>
2020-10-23 18:14:46 +0300

    first try

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,46 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
-from .models import Post, Comment
+from .models import Comment, Follow, Group, Post
+
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
 
     class Meta:
-        fields = ('id', 'text', 'author', 'pub_date')
         model = Post
+        fields = ('id', 'text', 'author', 'pub_date')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
-        fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+        fields = ('id', 'text', 'author', 'post', 'created')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'title')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
+    following = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
--- api/views.py
+++ api/views.py
@@ -1,1 +1,62 @@
-# TODO:  Напишите свой вариант
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django_filters import rest_framework as filters
+from rest_framework import generics, viewsets, serializers
+
+from rest_framework.filters import SearchFilter
+
+from .models import Follow, Group, Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (
+    CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
+)
+
+
+User = get_user_model()
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ('group',)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post_id = self.kwargs['post_id']
+        post = get_object_or_404(Post, pk=post_id)
+        return post.comments
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['post_id']
+        post = get_object_or_404(Post, pk=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowList(generics.ListCreateAPIView):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['=user__username', '=following__username']
+
+    def perform_create(self, serializer):
+        username = self.request.data.get('following')
+        # не понял как это сделать красиво..
+        if not User.objects.filter(username=username).exists() or \
+           Follow.objects.filter(user=self.request.user, following__username=username).exists():
+            raise serializers.ValidationError("This user doesn't exists")
+        following = User.objects.get(username=username)
+        serializer.save(user=self.request.user, following=following)
